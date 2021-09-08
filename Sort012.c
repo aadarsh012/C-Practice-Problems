7acf37bd8cbd9fa64a6c067896d9b1422935f54d
aadarsh012 <aadarsh012@gmail.com>
2021-09-08 19:22:10 +0530

    Optimized

--- Sort012.c
+++ Sort012.c
@@ -1,3 +1,21 @@
+/*Q-> Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+*/
+
+// Solution.
+
 #include <stdio.h>
 
 void swap(int *a, int *b)
@@ -8,7 +26,7 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
-int sort012(int *a, int n)
+void sort012(int *a, int n)
 {
     int c = 0;
     for (int i = 0; i < n; i++)
